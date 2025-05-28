01d8c447a605f909bb6e89c26085e947f849ee69
CodeSandbox <support@codesandbox.io>
2025-05-28 02:10:06 +0000

    Add: Logo to navigation header

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from 'next/image';
 
 export default function Navigation() {
   return (
@@ -6,7 +7,14 @@ export default function Navigation() {
       <nav className="fixed top-0 left-0 right-0 z-50 bg-white border-b">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
-            <div className="flex items-center">
+            <div className="flex items-center space-x-3">
+              <Image
+                src="/logo.svg"
+                alt="UNSW Entrepreneurs' Society Logo"
+                width={32}
+                height={32}
+                className="w-8 h-8"
+              />
               <a href="/" className="text-black font-bold">
                 UNSW Entrepreneurs' Society
               </a>
