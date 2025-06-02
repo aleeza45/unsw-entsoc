5416a24ee780e26c961d88fda756e6597b36a7bd
CodeSandbox <support@codesandbox.io>
2025-06-02 05:11:14 +0000

    Fix: Switch to img tag for SVG logo display

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from 'next/image';
 
 export default function Navigation() {
   return (
@@ -8,11 +7,9 @@ export default function Navigation() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center space-x-4">
-              <Image
+              <img
                 src="/logo.svg"
                 alt="UNSW Entrepreneurs' Society Logo"
-                width={40}
-                height={40}
                 className="w-10 h-10"
               />
               <a href="/" className="text-black font-bold">
