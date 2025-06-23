3faf1a84a73dc42c62fc104f3eb1166d1d904973
User Name <user@example.com>
2025-06-23 01:18:53 +0000

    Add Meet Our Team section with see more functionality

---
+++ user-workspace/src/app/page.tsx
@@ -0,0 +1,1 @@
+import React from 'react'\nimport TeamSection from './TeamSection'\n\nexport default function Home() {\n  return (\n    <div className='min-h-screen'>\n      <TeamSection />\n    </div>\n  )\n}
